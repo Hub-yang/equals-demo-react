db2f14435897e5f62d34445be756d30a19aef82a
Hubery Yang <78254474+Hub-yang@users.noreply.github.com>
2026-02-27 08:28:47 +0800

    feat: optimize useDark hook

--- src/hooks/useDark.ts
+++ src/hooks/useDark.ts
@@ -14,16 +14,24 @@ export default function useTheme() {
     return defaultThemeMode || ThemeMode.LIGHT
   })
 
+  // 确保初始状态正确应用到 DOM
   useEffect(() => {
-    const html = document.getElementsByTagName('html')
-    html[0].className = themeMode
+    const html = document.documentElement
+    if (html.className !== themeMode) {
+      html.className = themeMode
+    }
+  })
+
+  useEffect(() => {
+    const html = document.documentElement
+    html.className = themeMode
   }, [themeMode])
 
   function setThemeModeWithLocalStorage(mode: ThemeType) {
     setThemeMode(mode)
     localStorage.setItem(key, mode)
-    const html = document.getElementsByTagName('html')
-    html[0].className = mode
+    const html = document.documentElement
+    html.className = mode
   }
 
   return {
